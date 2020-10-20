f673506359acd6845b99cfa0f48df4dba56517fb
b34nb0 <fbhirakata@gmail.com>
2020-10-20 15:52:12 -0500

    Add setup for static assets in 'server.js'

--- server.js
+++ server.js
@@ -26,6 +26,16 @@ const notesRouter = require('./routes/api/notes');
 app.use('/api/users',usersRouter);
 app.use('/api/notes',notesRouter);
 
+// Serve static assets in production
+if(process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
+
 
 // Start up server
 app.listen(port, () => {
